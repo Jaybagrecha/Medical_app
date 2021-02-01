274254846212041a51be00c889c75e87e221d75f
Jay Bagrecha <57600618+Jaybagrecha@users.noreply.github.com>
2021-02-01 15:22:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ UPLOAD_FOLDER = 'uploads'
 STATIC_FOLDER = 'static'
 
 @app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/cancer")
 def cancer():
